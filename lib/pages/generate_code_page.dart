2fb956660613324c126e02859b1ce473fd1689e5
vasudholakiya16 <122475789+vasudholakiya16@users.noreply.github.com>
2024-07-29 22:47:23 +0530

    first commit

---
+++ lib/pages/generate_code_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:pretty_qr_code/pretty_qr_code.dart';
+
+class GenerateCodePage extends StatefulWidget {
+  const GenerateCodePage({super.key});
+
+  @override
+  State<GenerateCodePage> createState() => _GenerateCodePageState();
+}
+
+class _GenerateCodePageState extends State<GenerateCodePage> {
+  String? qrData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Generate QR Code'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              onSubmitted: (value) {
+                setState(() {
+                  qrData = value;
+                });
+              },
+            ),
+            if (qrData != null) PrettyQrView.data(data: qrData!),
+          ],
+        ),
+      ),
+    );
+  }
+}
